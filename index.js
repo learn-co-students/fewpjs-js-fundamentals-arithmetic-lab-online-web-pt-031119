e7dab672cc2094e02a784bb7309dd25a1be690b3
Alex Merecka <merecka@gmail.com>
2019-10-23 14:34:06 -0600

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const num1 = 9
+const num2 = 7
 
-// Write your code here
+let multiply = num1 * num2 - 1
+
+let random = Math.floor(Math.random()) + 0.001
+
+let mod = 16 % 6
+
+let max = Math.max (1, 2, 4, 5, 20)
