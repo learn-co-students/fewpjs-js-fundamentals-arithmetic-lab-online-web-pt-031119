02e1cd79224bb7581868052f5c201464affba1e5
Danieleclima <daniel.echuaca.lima@gmail.com>
2019-10-27 20:47:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+let num1 = 62**(1/2)
+let num2 = 62**(1/2)
+let multiply = num1 * num2
+
+let random = Math.ceil(Math.random() * 10)
+
+let mod = 10%6
+
+let max = Math.max(0,20)
